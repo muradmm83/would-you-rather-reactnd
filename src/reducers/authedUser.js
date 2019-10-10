f338489bcaaef42e8c724b3743708457d82c549d
Murad Bin Makhashin <muradmm83@gmail.com>
2019-10-10 22:05:31 +0300

    feat: add authedUser reducer

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,12 +1,12 @@
 import { LOG_IN, LOG_OUT } from '../actions/authedUser';
 
-export default (state = {}, action) => {
+export default (state = null, action) => {
     switch (action.type) {
         case LOG_IN:
             return { ...state, ...action.user };
 
         case LOG_OUT:
-            return {}
+            return null;
 
         default:
             return state;
