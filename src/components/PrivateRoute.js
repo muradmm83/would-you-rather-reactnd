53ea02ef44032bb88d6b720be352d010ce5b4a54
Murad Bin Makhashin <muradmm83@gmail.com>
2019-10-13 21:32:22 +0300

    refactor: fix rout guard

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export default ({ children, ...rest }) => {
+export default ({ component: Component, ...rest }) => {
     const { authedUser } = rest;
 
     return (
         <Route
             {...rest}
-            render={() => authedUser ? children : <Redirect to="/login" />}
+            render={(props) => authedUser ? <Component {...props} /> : <Redirect to="/login" />}
         />
     )
 }
