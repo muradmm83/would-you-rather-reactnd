121a05b52236035c61849a23333240f1e40acc50
Murad Bin Makhashin <muradmm83@gmail.com>
2019-10-14 22:35:49 +0300

    feat: add poll submit

---
+++ src/components/PollSubmit.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { handleAnswerQuestion } from '../actions/questions';
+
+/* ToDo -- handle submit and probably change functional to class component */
+
+class PollSubmit extends Component {
+    state = {
+        submitComplete: false,
+        selectedAnswer: null
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const { dispatch, authedUser, question } = this.props;
+        const { selectedAnswer } = this.state;
+
+        dispatch(handleAnswerQuestion(authedUser.id, question.id, selectedAnswer));
+
+        this.setState({
+            submitComplete: true,
+            selectedAnswer: null
+        });
+    }
+
+    onChange = e => {
+        this.setState({
+            submitComplete: false,
+            selectedAnswer: e.target.value
+        });
+    }
+
+    render() {
+        const { author, question } = this.props;
+        const { selectedAnswer, submitComplete } = this.state;
+
+        if (submitComplete) {
+            return (<Redirect to={`/poll/${question.id}`} />);
+        }
+
+        return (
+            <div className="card center">
+                <div className="title">{`${author.name} asks:`}</div>
+
+                <div className="body">
+
+                    <img className="avatar" src={author.avatarURL} />
+
+                    <div className="content border-left">
+                        <h3>Would you rather ...</h3>
+                        <form onSubmit={this.onSubmit}>
+                            <p>
+                                <input type="radio" name="answer" value="optionOne" onChange={this.onChange} /> {question.optionOne.text}
+                            </p>
+                            <p>
+                                <input type="radio" name="answer" value="optionTwo" onChange={this.onChange} /> {question.optionTwo.text}
+                            </p>
+
+                            <button type="submit" disabled={selectedAnswer === null}>Submit</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(({ authedUser, users }) => ({ authedUser: users[authedUser] }))(PollSubmit);
