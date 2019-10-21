bf55b5e0fcb5b77d7c86d499290729b1dfcf4f70
Murad Makhashin <muradmm83@gmail.com>
2019-10-21 09:59:31 +0300

    fix: change 'class' attribute to 'className'

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -9,22 +9,22 @@ export default props => {
             {users.map(u => (
                 <div className="card center" key={u.id}>
 
-                    <div class="body">
+                    <div className="body">
 
                         <img src={u.avatarURL} className="avatar" alt={u.name} />
 
-                        <div class="content border-left border-right">
+                        <div className="content border-left border-right">
                             <h3>{u.name}</h3>
 
                             <div className="flex">
-                                <span class="flex-grow">Answered question</span>
+                                <span className="flex-grow">Answered question</span>
                                 <span>{u.answers}</span>
                             </div>
 
                             <hr />
 
                             <div className="flex">
-                                <span class="flex-grow">Created question</span>
+                                <span className="flex-grow">Created question</span>
                                 <span>{u.questions}</span>
                             </div>
                         </div>
@@ -32,7 +32,7 @@ export default props => {
                         <div className="card">
                             <span className="title">Score</span>
 
-                            <span class="score">{u.answers + u.questions}</span>
+                            <span className="score">{u.answers + u.questions}</span>
                         </div>
 
                     </div>
